79c9a6c492c553747ff907c9a76c0c13a3139687
Jayden Man <manchunkit17@gmail.com>
2023-03-27 15:52:14 +0800

    models and database

--- Flask/test.py
+++ Flask/test.py
@@ -1,5 +1,42 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+# This code sets the URI (Uniform Resource Identifier) for the SQLite database about.db 
+# in a Flask web application using SQLAlchemy. 
+# The app refers to the Flask application created earlier in the code. 
+# This URI will be used by SQLAlchemy to connect to the SQLite database.
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///about.db'
+
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False, unique=True)
+    barcode = db.Column(db.String(12), nullable=False, unique=True)
+    price = db.Column(db.Integer, nullable=False, unique=False)
+    description = db.Column(db.String(1024), nullable=False, unique=False)
+
+    def __repr__(self):
+        return f'Item {self.name}'
+    # Python shell python
+    # from market import app,db
+    # app.app_context().push()
+    # db.create_all()
+
+    # for item in Item.query.all():
+# ...     item.name
+# ...     item.barcode
+# ...     item.price
+# ...     item.id
+
+#     >>> for item in Item.query.filter_by(price=500):
+# ...     item.name
+
+
+
+    
+
 
 @app.route('/')
 @app.route('/home')
@@ -10,13 +47,7 @@ def home_page():
 @app.route('/About')
 @app.route('/about')
 def about_page():
-    items = [
-        {'id':1,'name':'Phone','barcode':'893212299897','price':1000},
-        {'id':2,'name':'Laptop','barcode':'123985473165','price':2000},
-        {'id':3,'name':'TV','barcode':'231985128446','price':3000},
-
-
-        ]
+    items = Item.query.all()
 
     return render_template('about_New.html',items=items)
 
