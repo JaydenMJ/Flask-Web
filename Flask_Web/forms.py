210e8d21f83b73a958171b349bbc07e0ca90306d
Jayden Man <manchunkit17@gmail.com>
2023-04-12 00:04:27 +0800

    wtforms.validators

--- Flask_Web/forms.py
+++ Flask_Web/forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-
+from wtforms.validators import length, Email, EqualTo, DataRequired
 
 class RegisterForm(FlaskForm):
-    username = StringField(label='Username: ')
-    email = StringField(label='Email: ')
-    password = PasswordField(label='Password: ')
-    confirm_password = PasswordField(label='Confirm Password: ')
+    username = StringField(label='Username: ', validators=[length(min=2, max=30),DataRequired()])
+    email = StringField(label='Email: ',validators=[Email(), DataRequired()])
+    password = PasswordField(label='Password: ', validators=[length(min=6), DataRequired()])
+    confirm_password = PasswordField(label='Confirm Password: ', validators=[EqualTo('password'), DataRequired()])
     submit = SubmitField(label='Sign Up')
 
 
--- Flask_Web/routes.py
+++ Flask_Web/routes.py
@@ -1,6 +1,6 @@
-from Flask_Web import app
-from flask import render_template
-from Flask_Web.models import Item
+from Flask_Web import app,db
+from flask import render_template, redirect, url_for
+from Flask_Web.models import Item, User
 from Flask_Web.forms import RegisterForm
 
 @app.route('/')
@@ -16,7 +16,17 @@ def about_page():
     items = Item.query.all()
     return render_template('about_New.html',items=items)
 
-@app.route('/Register')
+@app.route('/Register', methods=['GET','POST'])
 def register_page():
     form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username=form.username.data,
+                              email=form.email.data,
+                              password_hash=form.password.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('about_page'))
+    if form.errors != {}: # No error 
+        for err_msg in form.errors.values():
+            print(f'There was an error with creating a user: {err_msg}')
     return render_template('register.html',form=form)
