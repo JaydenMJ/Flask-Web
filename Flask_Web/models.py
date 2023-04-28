db9f57cdc5f3be2636503bf560c4267fc489c164
Jayden Man <manchunkit17@gmail.com>
2023-04-29 01:57:37 +0800

    flask_login UserMixin, user_loader, prettier budget

--- Flask_Web/models.py
+++ Flask_Web/models.py
@@ -1,7 +1,14 @@
-from Flask_Web import db
+from Flask_Web import db,login_manager
 from Flask_Web import bcrypt
+from flask_login import UserMixin
 
-class User(db.Model):
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model,UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), nullable=False, unique=True)
     email = db.Column(db.String(50), nullable=False, unique=True)
@@ -10,6 +17,14 @@ class User(db.Model):
     items = db.relationship('Item',backref='owned_user', lazy = True)
     
     @property
+    def prettier_budget(self):
+        if len(str(self.budget))>=4:
+            number_with_commas = "{:,}$".format(self.budget)
+            return number_with_commas
+        else:
+            return f'{self.budget:}$'
+
+    @property
     def password(self):
         return self.password
     
@@ -17,6 +32,10 @@ class User(db.Model):
     def password(self,plain_text_password):
         self.password_hash = bcrypt.generate_password_hash(plain_text_password).decode('utf-8')
 
+    def check_password_correction(self,attempted_password):
+        return bcrypt.check_password_hash(self.password_hash,attempted_password)
+            
+        
 
 
 class Item(db.Model):
