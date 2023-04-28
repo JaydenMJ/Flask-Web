04368d10e4f817b730e55aed6cefb06b3208133a
Jayden Man <manchunkit17@gmail.com>
2023-04-29 02:00:22 +0800

    login_user in login page

--- Flask_Web/routes.py
+++ Flask_Web/routes.py
@@ -2,6 +2,7 @@ from Flask_Web import app,db
 from flask import render_template, redirect, url_for, flash,get_flashed_messages
 from Flask_Web.models import Item, User
 from Flask_Web.forms import RegisterForm, LoginForm
+from flask_login import login_user
 
 @app.route('/')
 @app.route('/home')
@@ -34,5 +35,16 @@ def register_page():
 @app.route('/Login', methods=['GET','POST'])
 def login_page():
     form = LoginForm()
+    if form.validate_on_submit():
+        attempted_user = User.query.filter_by(username = form.username.data).first()
+        if attempted_user and attempted_user.check_password_correction(
+            attempted_password = form.password.data
+        ):
+            login_user(attempted_user)
+            flash(f'Success! You are logged in as: {attempted_user.username}', category='success')
+            return redirect(url_for('about_page'))
+        else:
+            flash('Username and password are not match! Please try again', category='danger')
+
 
     return render_template('login.html',form=form)
